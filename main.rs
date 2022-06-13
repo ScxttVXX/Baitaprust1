0d2fb5860c1f91e29eec940476361407eb2365c2
ScxttVXX <107401412+ScxttVXX@users.noreply.github.com>
2022-06-13 16:51:31 +0700

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,34 +1,14 @@
-fn main() {
-    let org_arr:[i32;8] = [1,2,3,5,6,8,10,11];
-    let sub_arr:[i32;3] = [6,8,10]; 
+use std::io;
 
-    let check: bool = is_a_contain_b(&org_arr,&sub_arr);
-    println!("{}",check);
-
-  
-}
-
-fn is_a_contain_b(A: &[i32], B: &[i32]) -> bool{
+fn main () {
+    println!("Enter substring:");
+    let mut guess = String::new();
  
-    let (mut i, mut j):(i32,i32) = (0,0);
-    let  n:i32 = A.len().try_into().unwrap();
-    let  m:i32 = B.len().try_into().unwrap();
-  //println!("{}", n);
-    while i < n && j < m {
-        if A[i as usize] == B[j as usize]{
-              i += 1;
-              j += 1;
-            if j == m {
-                return true;
-            }
-        }else {
-            i = i + 1;
-            j = 0;
-        }
-         
-    }
-   return false;
-   
+    io::stdin().read_line(&mut guess).expect("failed to readline");
+ 
+    
+    let chuoi = String::from("This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal. This is a regular paragraph with the default style of Normal.");
+    println!("{}",chuoi.matches(&guess.trim()).count());
+
+    println!("{}", guess);
 }
-
-
